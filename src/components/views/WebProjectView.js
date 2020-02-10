1106dc1de4ebc7df7fedf7b2fccec9909f7960c3
Daniel Palacio <dpalacio@buffalo.edu>
2020-02-10 12:46:40 -0500

    Vidjot pictures

--- src/components/views/WebProjectView.js
+++ src/components/views/WebProjectView.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import { Image, Button } from "react-bootstrap";
-
-export class WebProjectView extends  Component {
-
-
-    render(){
-        return(
-            <div className="webProjectCell">
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
-
+import projectImage from "../images/vidjotlaptop.png";
+export class WebProjectView extends Component {
+  render() {
+    return (
+      <div className='webProjectCell'>
+        <h1>{this.props.title}</h1>
+        <Image src={projectImage} fluid />
+      </div>
+    );
+  }
 }
 export default WebProjectView;
