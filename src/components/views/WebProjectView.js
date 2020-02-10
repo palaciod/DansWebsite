7362ff33fdfa704f194a497baa1c4315b5de8933
Dan <dpalacio@buffalo.edu>
2020-02-10 13:37:00 -0500

    vidjot

--- src/components/views/WebProjectView.js
+++ src/components/views/WebProjectView.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Image, Button } from "react-bootstrap";
-import projectImage from "../images/vidjotlaptop.png";
+import projectImage from "../images/vidjotlaotop.png";
 export class WebProjectView extends Component {
   render() {
     return (
       <div className='webProjectCell'>
         <h1>{this.props.title}</h1>
-        <Image src={projectImage} fluid />
+        <div className="iosImage" style={{height: "80%", width: "80%"}}>
+            <Image src={projectImage} fluid/>
+        </div>
       </div>
     );
   }
